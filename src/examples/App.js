74a46f8038e007b46fe84a1ff0ecfd16cefe0028
ishan-dongol <ishan@fusemachines.com>
2020-03-04 15:15:03 +0545

    changed draft js to 0.10.0

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,21 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CustomInlineToolbarEditor from '../lib';
 import {EditorState} from 'draft-js';
-const App = () => {
-    const [editorState,setEditorState] = useState()
-    useEffect(()=>{
-setEditorState(EditorState.createEmpty())
-    },[])
-    return (
-        <React.Fragment>
-            {editorState ? <CustomInlineToolbarEditor
-            editorState={editorState}
-            imageButtonClassName={'btn btn-sm btn-green'}
-            onChange={setEditorState}
-            // imageUploadFn={this.props.uploadImage}
-            />:'Loading...'}
-        </React.Fragment>
-    )
-}
+// import draftToHtml from 'draftjs-to-html';
 
+class App extends React.Component {
+    state={
+        editorState: EditorState.createEmpty()
+    }
+    // customEntityTransform = (entity, text) => {
+    //     switch (entity.type) {
+    //       case 'INLINETEX':
+    //         return '';
+  
+    //       case 'KateX':
+    //         return katex.renderToString(entity.data.value, {
+    //           throwOnError: false,
+    //         });
+  
+    //       default:
+    //         return '';
+    //     }
+    //   };
+    setEditorState = (editorState) =>{
+        if(editorState){
+            // const rawContent = editorState.getCurrentContent()
+            // const draft =draftToHtml(rawContent, {}, false, this.customEntityTransform)
+            // console.log(draft)
+        this.setState({editorState})
+        }
+    }
+    render(){
+        return (
+            <React.Fragment>
+                {this.state.editorState ? <CustomInlineToolbarEditor
+                editorState={this.state.editorState}
+                imageButtonClassName={'btn btn-sm btn-green'}
+                onChange={this.setEditorState}
+                // imageUploadFn={this.props.uploadImage}
+                />:'Loading...'}
+            </React.Fragment>
+        )
+    }
+}
 export default App;
