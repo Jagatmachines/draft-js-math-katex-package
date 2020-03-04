8b4f8f4f0ed505e57a8feb6b92ebafdf7adf0b01
ishan-dongol <ishan@fusemachines.com>
2020-03-04 15:51:49 +0545

    package update

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import CustomInlineToolbarEditor from '../lib';
-import {EditorState} from 'draft-js';
-// import draftToHtml from 'draftjs-to-html';
+import {EditorState, convertToRaw} from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
 
 class App extends React.Component {
     state={
         editorState: EditorState.createEmpty()
     }
-    // customEntityTransform = (entity, text) => {
-    //     switch (entity.type) {
-    //       case 'INLINETEX':
-    //         return '';
-  
-    //       case 'KateX':
-    //         return katex.renderToString(entity.data.value, {
-    //           throwOnError: false,
-    //         });
-  
-    //       default:
-    //         return '';
-    //     }
-    //   };
     setEditorState = (editorState) =>{
         if(editorState){
-            // const rawContent = editorState.getCurrentContent()
-            // const draft =draftToHtml(rawContent, {}, false, this.customEntityTransform)
-            // console.log(draft)
         this.setState({editorState})
         }
     }
