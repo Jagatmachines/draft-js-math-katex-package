6dbbb0a9fb01b6c772647c527e5a59bf0b9ad65d
ishan-dongol <ishan@fusemachines.com>
2020-03-04 14:48:03 +0545

    bug fix

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomInlineToolbarEditor from '../lib';
 import {EditorState} from 'draft-js';
 const App = () => {
-    const [editorState,setEditorState] = useState(EditorState.createEmpty())
+    const [editorState,setEditorState] = useState()
+    useEffect(()=>{
+setEditorState(EditorState.createEmpty())
+    },[])
     return (
         <React.Fragment>
-            <CustomInlineToolbarEditor
+            {editorState ? <CustomInlineToolbarEditor
             editorState={editorState}
             imageButtonClassName={'btn btn-sm btn-green'}
             onChange={setEditorState}
             // imageUploadFn={this.props.uploadImage}
-            />
+            />:'Loading...'}
         </React.Fragment>
     )
 }
